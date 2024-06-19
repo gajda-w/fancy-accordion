1b6f49dc664c592323626295032c20e5b0938a2c
Wojciech Gajda <wojciech.gajda@mirumee.com>
2024-06-19 22:22:47 +0200

    add accordion giff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { FancyAccordion } from "./components/fancy-accordion";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-24">
       <FancyAccordion title="Accordion Title" />
     </main>
   );
